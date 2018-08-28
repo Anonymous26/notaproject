5ff84deab35b7d7c878948335248f3ea91605a7e
Subham Banga <subhambanga26@gmail.com>
2018-08-28 18:00:35 +0530

    created encrypt

--- main.py
+++ main.py
@@ -1,27 +1,48 @@
 from Crypto.Cipher import AES
+from Crypto import Random
 import base64,os,uuid 
 import time,hashlib
 
 x = 0
 dataVal = float(input("enter value upto 2 decimal places"))
+ownerName = input("enter the name of the owner")
+prOwnerId = 1
 class node:
   def __init__(self,gen):
     if gen is 0:
-      global x,dataVal
+      global x,dataVal,ownerName
       x = x + 1
       self.timeStamp = time.time()
       self.nodeId = uuid.uuid1().int>>32
       self.nodeNum = x
       self.refNodeId = None
-      self.childRefNodeId = None
-      self.genRefNodeId = None
-      self.data = self.encrypt(val = dataVal)
+      self.childRefNodeId = ''
+      self.genRefNodeId = ''
+      self.data = self.encrypt(val = dataVal, owner = ownerName)
       self.hashVal = hashlib.md5(str([self.timestamp, self.data, self.nodeNum, self.nodeId, self.refNodeId, self.childRefNodeId, self.genRefNodeId]))
 
 
 
-  def encrypt(val):
-    pass
+  def encrypt(self, val, owner, ownerId):
+    hashId = hashlib.md5(str([owner, val, ownerId, self.nodeId]).encode()).hexdigest()
+
+    if self.refNodeId is not None:
+      parentId = self.refNodeId
+
+    msg = str([owner, val, ownerId, hashId])
+    secret_key = base64.b64decode(ownerId)
+    cipher = AES.new(secret_key)
+    # pad the private_msg
+	  # because AES encryption requires the length of the msg to be a multiple of 16
+    padded_private_msg = msg + ("-" * ((16-len(msg)) % 16))
+	  # use the cipher to encrypt the padded message
+    encrypted_msg = cipher.encrypt(padded_private_msg)
+	  # encode the encrypted msg for storing safely in the database
+    encode_encrypt_msg = base64.b64encode(encrypted_msg)
+    return encode_encrypt_msg
+
+
+
 
   def get_secret_key():
     AES_key_length = 32
@@ -33,5 +54,9 @@ class node:
     self.nodeNum = x
     self.timeStamp = time.time()
     self.data = self.encrypt()
+  
+genesisNode = node(0)
+
+def new_user():
   
   
